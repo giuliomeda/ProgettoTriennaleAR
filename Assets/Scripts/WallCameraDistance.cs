2fdfdfc544e5fee63d989e5e54d2e75ed964aee8
Giulio Meda <giulio.meda@studenti.unipd.it>
2023-01-02 21:18:51 +0100

    Aggiunta implementazione per la seconda parte della misura e per la scrittura della dimensione calcolata su file csv

--- Assets/Scripts/WallCameraDistance.cs
+++ Assets/Scripts/WallCameraDistance.cs
@@ -32,9 +32,14 @@ public class WallCameraDistance : MonoBehaviour
     private Button dismissButton;
 
     [SerializeField]
+    private Button continueButton;
+
+    [SerializeField]
     private GameObject resultBox;
 
-    private float DistanceWallCamera;
+    private float[] DistanceWallCamera = new float[] {0.0f,0.0f,0.0f};
+
+    private int indexOfDistanceArray = 0;
 
     private ARRaycastManager arRaycastManager;
 
@@ -48,7 +53,11 @@ public class WallCameraDistance : MonoBehaviour
 
     private GameObject startPoint;
 
-    private ARPlane selectedPlane;
+    private GameObject endPoint;
+
+    private ARPlane firstSelectedPlane;
+
+    private ARPlane secondSelectedPlane;
 
     [SerializeField]
     private Text Distance;
@@ -62,31 +71,49 @@ public class WallCameraDistance : MonoBehaviour
     [SerializeField]
     private Text PlaneCoord;
 
+    public static string filename = "DimensionFile";
+
     public void RestartScene(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     void Awake() 
     {
         startPoint = Instantiate(measurePointPrefab, Vector3.zero, Quaternion.identity);
+        endPoint = Instantiate(measurePointPrefab, Vector3.zero, Quaternion.identity);
 
         arRaycastManager = GetComponent<ARRaycastManager>();
 
         m_ArPlaneManager = GetComponent<ARPlaneManager>();        
 
         startPoint.SetActive(false);
+        endPoint.SetActive(false);
 
         distanceText.gameObject.SetActive(false);
 
         dismissButton.onClick.AddListener(Dismiss);
 
+        continueButton.onClick.AddListener(moveIndexAndReactivatePlaneDetection);
+
     }
     private void Dismiss() => instructionCanvas.SetActive(false);
+
+    private void moveIndexAndReactivatePlaneDetection(){
+        //ri-abilito il plane detection per la seconda misurazione
+        m_ArPlaneManager.enabled = true;
+
+        //incremento indice array per salvare la seconda misura 
+        indexOfDistanceArray++;
+
+        //disattivo il pulsante 
+        continueButton.gameObject.SetActive(false);
+    }
+
     public void stopPlanesDetection(){
         m_ArPlaneManager.enabled = false;
 
-        if (!m_ArPlaneManager.enabled){                                     // se è appena stato selezionato un piano e quindi m_ArPlaneManager è appena stato disattivato (è in pausa la plane detection) allora distruggo tutti i piani tranne quello selezionato dall'utente
+        if (!m_ArPlaneManager.enabled){
             foreach(var plane in m_ArPlaneManager.trackables){
-                if (plane != selectedPlane)
+                if (plane != firstSelectedPlane && plane != secondSelectedPlane)
                     plane.gameObject.SetActive(false);
             }
             
@@ -98,29 +125,65 @@ public class WallCameraDistance : MonoBehaviour
 
     private void calculateDistanceAndDisplay(){
         if (!m_ArPlaneManager.enabled){
+            if (indexOfDistanceArray == 0){
+                m_ARSessionOrigin.MakeContentAppearAt(m_ARSessionOrigin.transform, arCameraManager.transform.position, arCameraManager.transform.rotation); //faccio in modo di settare la posizione della camera nell'origine
 
-            m_ARSessionOrigin.MakeContentAppearAt(m_ARSessionOrigin.transform, arCameraManager.transform.position, arCameraManager.transform.rotation); //faccio in modo di settare la posizione della camera nell'origine
-
-            Vector3 VectorForAlignement = new Vector3(arCameraManager.transform.position.x, arCameraManager.transform.position.y, selectedPlane.transform.position.z);
+                Vector3 VectorForAlignement = new Vector3(arCameraManager.transform.position.x, arCameraManager.transform.position.y, firstSelectedPlane.transform.position.z);
             
-            startPoint.SetActive(true);
-            startPoint.transform.SetPositionAndRotation(VectorForAlignement, arCameraManager.transform.rotation);
+                startPoint.SetActive(true);
+                startPoint.transform.SetPositionAndRotation(VectorForAlignement, arCameraManager.transform.rotation);
 
-            DistanceWallCamera = Vector3.Distance(VectorForAlignement, arCameraManager.transform.position); // calcolo la distanza
+                DistanceWallCamera[indexOfDistanceArray] = Vector3.Distance(VectorForAlignement, arCameraManager.transform.position); // calcolo la distanza
 
-            distanceText.gameObject.SetActive(true);
-            distanceText.gameObject.transform.position = hits[0].pose.position;
-            distanceText.gameObject.transform.rotation = arCameraManager.transform.rotation;
+                /*distanceText.gameObject.SetActive(true);
+                distanceText.gameObject.transform.position = hits[0].pose.position;
+                distanceText.gameObject.transform.rotation = arCameraManager.transform.rotation;*/
 
-            //print result in the box
-            Distance.text = $"Distance: {DistanceWallCamera}" ;
-            PlaneCoord.text =$"Plane: {selectedPlane.transform.position}";
-            RefPointCoord.text = $"Sphere: {startPoint.transform.position}";
-            //print the 3d text 
-            distanceText.text = $"Distance: {DistanceWallCamera.ToString("F2")} meters";
+                //print result in the box
+                Distance.text = $"Distance: {DistanceWallCamera[indexOfDistanceArray]}" ;
+                PlaneCoord.text =$"Plane: {firstSelectedPlane.transform.position}";
+                RefPointCoord.text = $"Sphere: {startPoint.transform.position}";
+                //print the 3d text 
+                //distanceText.text = $"Distance: {DistanceWallCamera[indexOfDistanceArray].ToString("F2")} meters";
+                
+                continueButton.gameObject.SetActive(true);
+                
+            }
+            
+            if (indexOfDistanceArray == 1){
+                m_ARSessionOrigin.MakeContentAppearAt(m_ARSessionOrigin.transform, arCameraManager.transform.position, arCameraManager.transform.rotation); //faccio in modo di settare la posizione della camera nell'origine
+
+                Vector3 VectorForAlignement = new Vector3(arCameraManager.transform.position.x, arCameraManager.transform.position.y, secondSelectedPlane.transform.position.z);
+            
+                endPoint.SetActive(true);
+                endPoint.transform.SetPositionAndRotation(VectorForAlignement, arCameraManager.transform.rotation);
+
+                DistanceWallCamera[indexOfDistanceArray] = Vector3.Distance(VectorForAlignement, arCameraManager.transform.position); // calcolo la distanza
+
+                /*distanceText.gameObject.SetActive(true);
+                distanceText.gameObject.transform.position = hits[0].pose.position;
+                distanceText.gameObject.transform.rotation = arCameraManager.transform.rotation;*/
+
+                //print result in the box
+                Distance.text = $"Distance: {DistanceWallCamera[indexOfDistanceArray]}" ;
+                PlaneCoord.text =$"Plane: {secondSelectedPlane.transform.position}";
+                RefPointCoord.text = $"Sphere: {endPoint.transform.position}";
+                //print the 3d text 
+                //distanceText.text = $"Distance: {DistanceWallCamera[indexOfDistanceArray].ToString("F2")} meters";
+                
+            }
         }
     }
     
+    private void calculateTotalDimensionAndPrint(){
+        indexOfDistanceArray++;
+        DistanceWallCamera[2] = DistanceWallCamera[0] + DistanceWallCamera[1];
+        distanceText.gameObject.SetActive(true);
+        distanceText.gameObject.transform.position = hits[0].pose.position;
+        distanceText.gameObject.transform.rotation = arCameraManager.transform.rotation;
+        distanceText.text = $"Total dimension: {DistanceWallCamera[indexOfDistanceArray].ToString("F2")} meters";
+    }
+
     void Update()
     {   
         if (instructionCanvas.gameObject.activeSelf){
@@ -151,18 +214,35 @@ public class WallCameraDistance : MonoBehaviour
 
                     if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
                     {
-                        selectedPLaneId = hits[0].trackableId; // ho l'id del piano selezionato
+                        if (indexOfDistanceArray==0){
+                            selectedPLaneId = hits[0].trackableId; // ho l'id del piano selezionato
 
-                        Pose hitPose = hits[0].pose;
-                        
-                        if ((selectedPlane = m_ArPlaneManager.GetPlane(selectedPLaneId)) != null){
+                            Pose hitPose = hits[0].pose;
                             
-                            stopPlanesDetection();
+                            if ((firstSelectedPlane = m_ArPlaneManager.GetPlane(selectedPLaneId)) != null){
+                                
+                                stopPlanesDetection();
+                                
+                            }
+                        }
+                        if (indexOfDistanceArray==1){
+                            selectedPLaneId = hits[0].trackableId; // ho l'id del piano selezionato
+
+                            Pose hitPose = hits[0].pose;
                             
+                            if ((secondSelectedPlane = m_ArPlaneManager.GetPlane(selectedPLaneId)) != null){
+                                
+                                stopPlanesDetection();
+                                
+                            }
                         }
                     }
                 }
             
+            }
+            if (DistanceWallCamera[0] != 0.0f && DistanceWallCamera[1] != 0.0f){
+                calculateTotalDimensionAndPrint();
+                WriteResultIntoFile.WriteFloatArray(DistanceWallCamera, filename);
             }
         }
         
