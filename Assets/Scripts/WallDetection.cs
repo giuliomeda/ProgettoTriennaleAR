435d4a18188f8fa38dcb8ae6e5971cb7c20f8016
Giulio Meda <giulio.meda@studenti.unipd.it>
2023-01-30 21:12:27 +0100

    Aggiunto pulsante che consente il ritorno al main menù, spostato al centro dello schermo pulsante per inizio wall detection. Modificato pannello contenente le coordinate della camera

--- Assets/Scripts/WallDetection.cs
+++ Assets/Scripts/WallDetection.cs
@@ -39,22 +39,13 @@ public class WallDetection : MonoBehaviour
     private Button resetSceneButton;
 
     [SerializeField]
+    private Button backToMenùButton;
+
+    [SerializeField]
     private GameObject resultBox;
 
     [SerializeField]
-    private Text Wall1;
-
-    [SerializeField]
     private Text CameraCoord;
-
-    [SerializeField]
-    private Text Wall2;
-
-    [SerializeField]
-    private Text Wall3;
-
-    [SerializeField]
-    private Text Wall4;
 
     [SerializeField]
     private RoomDimensionsController my_room;
@@ -70,6 +61,7 @@ public class WallDetection : MonoBehaviour
         startScanButton.onClick.AddListener(setOriginAndStartScan);
         saveWallButton.onClick.AddListener(saveWall);
         resetSceneButton.onClick.AddListener(resetScan);
+        backToMenùButton.onClick.AddListener(backToMainMenù);
 
         m_ARPlaneManager.enabled = false;       //cerco di partire con planedetection disattivato
     }
@@ -105,6 +97,10 @@ public class WallDetection : MonoBehaviour
     private void resetScan(){
         my_room.clearRoomForResetScan();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void backToMainMenù(){
+        SceneManager.LoadScene("MainMenù");
     }
 
     // Update is called once per frame
